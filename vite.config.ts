59cfad360dea2f0edc8ceb25593e09d7a4e213d0
Павел Александрович Бережной <bpa0607@yandex.ru>
2026-03-17 19:01:56 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-book-manager/' // Обязательно со слешами в начале и конце! Название репозитория
+  base: '/14_front/' // Обязательно со слешами в начале и конце! Название репозитория
 })
